f64314c438426f99987613fb4f5f2e505e7dedf7
r0808 <arthurdelophem3@gmail.com>
2022-10-19 16:15:13 +0200

    feat: create message in localhost database

--- public/javascripts/saveMessage.js
+++ public/javascripts/saveMessage.js
@@ -2,6 +2,22 @@ document.querySelector('.btn').addEventListener('click', (e) => {
     text = document.querySelector('.input').value
     if (text !== "") {
         console.log(text)
+        const message = {
+            username: 'Elon Musk',
+            message: text
+        }
+        fetch('http://localhost:3000/api/v1/messages', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(message),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+            }
+            );
 
     }
     else {
